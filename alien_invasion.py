02917345d91d30687edd4e1be1f63c1ffa26ebc9
天晨 袁 <904677070@qq.com>
2024-02-20 16:09:22 +0800

    飞船四向移动

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,24 +2,61 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """ 管理游戏资源和行为的类"""
 
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_height)
+        )
         pygame.display.set_caption("Alien Invasion")
+        
+        self.ship = Ship(self)
+
     def run_game(self):
         """游戏主循环"""
         while True:
-            #监视键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-           
-            #让最近绘制的屏幕可见
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        """响应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                if event.key == pygame.K_UP:
+                    self.ship.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                if event.key == pygame.K_UP:
+                    self.ship.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = False
+
+    def _update_screen(self):
+        """更新屏幕上的图像"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
     
 if __name__ == '__main__':
     ai = AlienInvasion()
