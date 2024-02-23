f90e97497dfc82e5449cf65af5f49ae508cf2847
天晨 袁 <904677070@qq.com>
2024-02-23 15:43:44 +0800

    重构飞船部分

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,23 +33,33 @@ class AlienInvasion:
                 sys.exit()
 
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-                if event.key == pygame.K_UP:
-                    self.ship.moving_up = True
-                elif event.key == pygame.K_DOWN:
-                    self.ship.moving_down = True
+                _check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
-                if event.key == pygame.K_UP:
-                    self.ship.moving_up = False
-                elif event.key == pygame.K_DOWN:
-                    self.ship.moving_down = False
+                _check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """响应按键按下"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+
+    def _check_keyup_events(self, event):
+        """相应按键松开"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
 
     def _update_screen(self):
         """更新屏幕上的图像"""
--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """初始化飞船"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # 加载飞船图像并获取其外接矩形
@@ -15,22 +16,34 @@ class Ship:
         # 对于每艘新飞船，都将其放在屏幕底部的中央
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # 位置
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
         # 移动标志
         self.moving_right = False
         self.moving_left = False
         self.moving_up = False
         self.moving_down = False
 
+        #飞船速度
+        self.ship_speed = self.settings.ship_speed
+
+
     def update(self):
         """根据移动标志调整飞船的位置"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
-        if self.moving_up:
-            self.rect.y -= 1
-        if self.moving_down:
-            self.rect.y += 1
+        # 更新飞船坐标
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.ship_speed
+            
+        self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         """在指定位置绘制飞船"""
